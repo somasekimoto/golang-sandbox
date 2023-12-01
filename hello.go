89a5e404572c511ff539ac0a6ba57d3baba540c6
somasekimoto <0421.soma@gmail.com>
2023-12-02 02:52:15 +0900

    Refactor Hello function to support multiple languages

--- hello.go
+++ hello.go
@@ -2,8 +2,23 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
-	return "Hello, " + name
+const spanish = "Spanish"
+const english = "English"
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+
+func Hello(name string, language ...string) string {
+	lang := english
+	if len(language) > 0 {
+		lang = language[0]
+	}
+	if name == "" {
+		name = "World"
+	}
+	if lang == spanish {
+		return spanishHelloPrefix + name
+	}
+	return englishHelloPrefix + name
 }
 
 func main() {
